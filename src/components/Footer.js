7bb16afb91ce6a12f37b68b8cad7c256de634239
James Ortiz <jameswortiz728@gmail.com>
2023-05-22 20:35:20 -0400

    Styled Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     return (
          <footer className="footer">
-            <div className="content-container">
+            <div className="container">
                 <h1><a href="https://www.jwortiz.dev/">Return to JWOrtiz.dev</a></h1>
             </div>
         </footer>
